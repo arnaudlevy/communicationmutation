58f8cc1ea13f7477a9dba94b072b5d0646304dda
Arnaud Levy <contact@arnaudlevy.com>
2020-10-27 09:17:39 +0100

    ruby 2.7.2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require "carrierwave/storage/fog"
 
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
-    config.storage :file
+    config.storage = :file
     config.asset_host = 'http://localhost:3000'
   else
     config.storage = :fog
