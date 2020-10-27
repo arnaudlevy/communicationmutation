3d2a03172ad5d62e365911b6440bf3fb1a1f421f
Arnaud Levy <contact@arnaudlevy.com>
2020-10-27 08:18:29 +0100

    fix puma

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 CarrierWave.configure do |config|
-  if Rails.env.test?
+  if Rails.env.development? || Rails.env.test?
     config.storage :file
     config.asset_host = 'http://localhost:3000'
   else
     config.storage = Fog
+    config.fog_provider = 'fog/aws'
     config.fog_directory = ENV['AWS_S3_BUCKET']
     config.fog_public = false
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_S3_ACCESS_KEY_ID'],
