5c2533f3e963d0305437e3fcf430b2b07a621e3b
Arnaud Levy <contact@arnaudlevy.com>
2021-01-22 09:37:10 +0100

    db

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_112347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
+  enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
